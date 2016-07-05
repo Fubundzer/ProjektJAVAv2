040fa992a8fee375380b4138cd3b288a11d5bfe4
Fubundzer <michal.r.94@interia.pl>
2016-07-06 00:01:50 +0200

    Nowy projekt mavenowy

---
+++ src/main/java/com/myapp/catalogMain.java
@@ -0,0 +1,10 @@
+package com.myapp;
+
+/**
+ * Created by Michał on 05.07.2016.
+ */
+public class catalogMain {
+    public static void main(String[] args ){
+        System.out.println("hello");
+    }
+}
